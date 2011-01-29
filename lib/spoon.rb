127aed2b985f78d90efe720dc82271c0dde89c03
Michael Rykov <mrykov@gmail.com>
2011-01-29 14:33:37 -0800

    Add specs and file_actions

--- lib/spoon.rb
+++ lib/spoon.rb
@@ -3,42 +3,97 @@ require 'ffi'
 module Spoon
   extend FFI::Library
   ffi_lib 'c'
-  
+
   # int
   # posix_spawn(pid_t *restrict pid, const char *restrict path,
   #     const posix_spawn_file_actions_t *file_actions,
   #     const posix_spawnattr_t *restrict attrp, char *const argv[restrict],
   #     char *const envp[restrict]);
-  
+
   attach_function :_posix_spawn, :posix_spawn, [:pointer, :string, :pointer, :pointer, :pointer, :pointer], :int
   attach_function :_posix_spawnp, :posix_spawnp, [:pointer, :string, :pointer, :pointer, :pointer, :pointer], :int
-  
+
+  # File actions
+  attach_function :_posix_spawn_file_actions_init,    :posix_spawn_file_actions_init,     [:pointer], :int
+  attach_function :_posix_spawn_file_actions_destroy, :posix_spawn_file_actions_destroy,  [:pointer], :int
+  attach_function :_posix_spawn_file_actions_adddup2, :posix_spawn_file_actions_adddup2,  [:pointer, :int, :int], :int
+  attach_function :_posix_spawn_file_actions_addclose,:posix_spawn_file_actions_addclose, [:pointer, :int], :int
+
   def self.spawn(*args)
-    spawn_args = _prepare_spawn_args(args)
-    _posix_spawn(*spawn_args)
-    spawn_args[0].read_int
+    _with_prepared_spawn_args(args) do |spawn_args|
+      _posix_spawn(*spawn_args)
+      spawn_args[0].read_int
+    end
   end
 
   def self.spawnp(*args)
-    spawn_args = _prepare_spawn_args(args)
-    _posix_spawnp(*spawn_args)
-    spawn_args[0].read_int
+    _with_prepared_spawn_args(args) do |spawn_args|
+      _posix_spawnp(*spawn_args)
+      spawn_args[0].read_int
+    end
   end
-  
+
   private
-  
-  def self._prepare_spawn_args(args)
+
+  def self._with_prepared_spawn_args(args)
+    # Make the arguments more like Kernel#spawn in Ruby 1.9
+    env = args.first.is_a?(Hash) ? ENV.merge(args.shift) : ENV
+    options = args.last.is_a?(Hash) ? args.pop : {}
+
     pid_ptr = FFI::MemoryPointer.new(:pid_t, 1)
 
     args_ary = FFI::MemoryPointer.new(:pointer, args.length + 1)
     str_ptrs = args.map {|str| FFI::MemoryPointer.from_string(str)}
     args_ary.put_array_of_pointer(0, str_ptrs)
 
-    env_ary = FFI::MemoryPointer.new(:pointer, ENV.length + 1)
-    env_ptrs = ENV.map {|key,value| FFI::MemoryPointer.from_string("#{key}=#{value}")}
+    env_ary = FFI::MemoryPointer.new(:pointer, env.length + 1)
+    env_ptrs = env.map {|key,value| FFI::MemoryPointer.from_string("#{key}=#{value}")}
     env_ary.put_array_of_pointer(0, env_ptrs)
-    
-    [pid_ptr, args[0], nil, nil, args_ary, env_ary]
+
+    _with_file_actions(options) do |actions_ptr|
+      yield [pid_ptr, args[0], actions_ptr, nil, args_ary, env_ary]
+    end
+  end
+
+  def self._with_file_actions(options)
+    # Initialize
+    actions_ptr = FFI::MemoryPointer.new(:pointer, 1) # FIXME
+    _posix_spawn_file_actions_init(actions_ptr)
+
+    # Connect the IO redirection
+    close_filenos = []
+    options.each do |src, dst|
+      src_no, dst_no = _fileno_for(src), _fileno_for(dst)
+
+      if src_no && dst_no
+        _posix_spawn_file_actions_adddup2(actions_ptr, dst_no, src_no)
+        close_filenos << src_no if src_no > 2
+        close_filenos << dst_no if dst_no > 2
+      end
+    end
+
+    # Close all non-standard descriptors (ala Kernel#spawn)
+    close_filenos.uniq.each do |fileno|
+      _posix_spawn_file_actions_addclose(actions_ptr, fileno)
+    end
+
+    # Call the action
+    out = yield(actions_ptr)
+
+    # Cleanup
+    _posix_spawn_file_actions_destroy(actions_ptr)
+
+    return out
+  end
+
+  def self._fileno_for(input)
+    if input.is_a?(Fixnum)
+      input
+    elsif input.respond_to?(:fileno)
+      input.fileno
+    elsif [:in, :out, :err].include?(input)
+      Kernel.const_get("STD#{input.to_s.upcase}").fileno
+    end
   end
 end
 
--- spec/spoon_spec.rb
+++ spec/spoon_spec.rb
@@ -16,4 +16,63 @@ describe Spoon do
     subject { Spoon.spawnp('sleep', '0.1') }
     it_should_behave_like 'a basic process'
   end
+
+  context 'with file actions' do
+    it 'should be able to read and write files' do
+      path = File.join(File.dirname(__FILE__), 'test.txt')
+      path_out = 'out.txt'
+
+      begin
+        src = File.new(path)
+        dst = File.new(path_out, 'w')
+        pid = Spoon.spawnp('cat', { :in => src, :out => dst })
+        Process.wait(pid)
+        src.close
+        dst.close
+        IO.read(path_out).should == IO.read(path)
+      ensure
+        File.unlink(path_out) if File.exist?(path_out)
+      end
+    end
+
+    it 'should be able to read and write IO.pipe' do
+      # FIXME: Need to figure out how to read from a IO.pipe
+      path = File.join(File.dirname(__FILE__), 'test.txt')
+      src = File.new(path)
+      dr, dw = IO.pipe
+      pid = Spoon.spawnp('cat', { :in => src, :out => dw })
+      Process.wait(pid)
+      src.close
+      dw.close
+      dr.read.should == IO.read(path)
+      dr.close
+    end
+
+    it 'should work with FD, IO, or symbols' do
+      [1, STDOUT, :out].each do |src|
+        r, w = IO.pipe
+        pid = Spoon.spawnp('echo', 'Hello', { src => w })
+        Process.wait(pid)
+        w.close
+        r.read.should == "Hello\n"
+        r.close
+      end
+    end
+  end
+
+  describe '_fileno_for' do
+    it 'should understand numbers' do
+      Spoon.send(:_fileno_for, 10).should == 10
+    end
+
+    it 'should understand file handles' do
+      Spoon.send(:_fileno_for, STDERR).should == STDERR.fileno
+    end
+
+    it 'should understand symbols' do
+      Spoon.send(:_fileno_for, :in) == STDIN.fileno
+      Spoon.send(:_fileno_for, :out) == STDOUT.fileno
+      Spoon.send(:_fileno_for, :err) == STDERR.fileno
+    end
+  end
 end
